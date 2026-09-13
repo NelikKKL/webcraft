3c5e3e708289bd6aa10d9143d88405ad953074c6
NelikKKL <smerdinmacsim@gmail.com>
2026-09-13 13:50:44 +0300

    Last Sync: 2026-09-13 13:50 (Mobile)

--- src/main/java/net/minecraft/client/oz.java
+++ src/main/java/net/minecraft/client/oz.java
@@ -65,7 +65,8 @@ public class oz {
         float f5 = 0.4f;
         float f6 = TrigLookup.b(f2 * (float)Math.PI * 2.0f) - 0.0f;
         if (f6 >= -0.0f - f5 && f6 <= -0.0f + f5) {
-            float f7 = (f6 - -0.0f) / f5 * 0.5f + 0.5f;
+            // Исправлено: заменен двойной минус, чтобы избежать SyntaxError в JS билде
+            float f7 = (f6 + 0.0f) / f5 * 0.5f + 0.5f;
             float f8 = 1.0f - (1.0f - TrigLookup.a(f7 * (float)Math.PI)) * 0.99f;
             f8 *= f8;
             this.h[0] = f7 * 0.3f + 0.7f;
@@ -105,4 +106,3 @@ public class oz {
         return null;
     }
 }
-
